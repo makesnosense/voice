7235453711234af0cc4f3286a97748a6d75a945b
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-09-12 11:33:01 +0300

    feat: messages layout

--- client/src/pages/room/Messages.tsx
+++ client/src/pages/room/Messages.tsx
@@ -1,24 +1,30 @@
 import type { Message } from '../../../../shared/types';
-
+import messagesStyles from './Messages.module.css';
 
 interface MessagesProps {
   messages: Message[];
 }
 
 export default function Messages({ messages }: MessagesProps) {
-  return <>
-    <div className="messages">
+  return (
+    <div className={messagesStyles.messages}>
       {messages.map((msg, index) => (
-        <div key={index} className="message">
-          <strong>{msg.userId}:</strong> {msg.text}
-          <div className="timestamp">
-            {new Date(msg.timestamp).toLocaleTimeString()}
+        <div key={index}>
+          <div className={messagesStyles.messageHeader}>
+            <strong>{msg.userId}</strong>
+            <span className={messagesStyles.timestamp}>
+              {new Date(msg.timestamp).toLocaleTimeString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+              })}
+            </span>
+          </div>
+          <div className={messagesStyles.messageText}>
+            {msg.text}
           </div>
         </div>
       ))}
     </div>
-
-  </>
-
-
+  );
 }
