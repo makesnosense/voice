163e8649876fcc1bcdf190bbfaf38a881d5a4887
Alex Berezovskiy <20311393+makesnosense@users.noreply.github.com>
2025-10-24 22:30:57 +0100

    fix(build): fixed client-side env variables passing for custom deployment

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,18 @@ set -e
 
 SCRIPT_DIR="$(dirname "$0")"
 PROJECT_ROOT="$SCRIPT_DIR/.."
+CUSTOM_DEPLOYMENT_DIR="$PROJECT_ROOT/custom-deployment"
+
+
+# load environment variables
+source "$CUSTOM_DEPLOYMENT_DIR/.env.production"
 
 cd "$PROJECT_ROOT"
 
 docker run --rm \
   -v .:/app \
+  -e VITE_TURN_SERVER_HOST=${VITE_TURN_SERVER_HOST} \
+  -e VITE_TURN_SERVER_PORT=${VITE_TURN_SERVER_PORT} \
   -w /app/client \
   node:alpine \
   sh -c "npm ci && npm run build"
