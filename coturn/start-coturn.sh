2517024cbe30ba022411e98ff461359bfb66b02c
Alex Berezovskiy <20311393+makesnosense@users.noreply.github.com>
2025-11-17 16:40:36 +0000

    feat(COTURN): allow more COTURN ports

--- coturn/start-coturn.sh
+++ coturn/start-coturn.sh
@@ -6,11 +6,15 @@ echo "🚀 Starting COTURN with CLI arguments..."
 exec turnserver \
   -n --log-file=stdout \
   --listening-port=$VITE_TURN_SERVER_PORT \
-  --min-port=49152 --max-port=49172 \
+  --min-port=49152 --max-port=65535 \
   --use-auth-secret \
   --static-auth-secret="$COTURN_SECRET" \
   --realm="$VITE_TURN_SERVER_HOST" \
   --server-name="$VITE_TURN_SERVER_HOST" \
   --no-cli \
   --no-multicast-peers \
-  --verbose
+  --verbose \
+  --stale-nonce=600 \
+  --max-allocate-lifetime=600 \
+  --channel-lifetime=600 \
+  --log-binding
