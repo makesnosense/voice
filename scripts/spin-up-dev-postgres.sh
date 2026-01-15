b5626287f22e577e17fbee37a06dcaa0b2001f7b
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-01-15 14:03:13 +0300

    chore: add --restart unless-stopped to dev postgres script.

--- scripts/spin-up-dev-postgres.sh
+++ scripts/spin-up-dev-postgres.sh
@@ -9,4 +9,5 @@ docker run -d \
   --env-file "$PROJECT_ROOT/.env.development" \
   -p 5432:5432 \
   -v voice-postgres-dev-data:/var/lib/postgresql/data \
+  --restart unless-stopped \
   postgres:16-alpine
