910fe04204d0938e7bda048e0a2cb49f84551505
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-02-16 21:58:01 +0300

    perf(client/exit-room-button): replace useNavigate with Link to eliminate router context subscription

--- client/src/pages/room/components/exit-room-button/ExitRoomButton.tsx
+++ client/src/pages/room/components/exit-room-button/ExitRoomButton.tsx
@@ -1,24 +1,17 @@
-import { useNavigate } from 'react-router';
+import { Link } from 'react-router';
 import { ChevronLeft } from 'lucide-react';
 import buttonStyles from '../../../../styles/Buttons.module.css';
-import exitButtonStyles from './ExitRoomButton.module.css'
+import exitButtonStyles from './ExitRoomButton.module.css';
 
 export default function ExitRoomButton() {
-  const navigate = useNavigate();
-
-  const handleExitRoom = () => {
-    navigate('/');
-  };
-
-
   return (
-    <button
-      onClick={handleExitRoom}
+    <Link
+      to="/"
       className={`${buttonStyles.button} ${buttonStyles.lightRed} ${buttonStyles.iconButton} 
       ${exitButtonStyles.exitRoomButton}`}
     >
       <ChevronLeft size={24} className={`${buttonStyles.icon} ${exitButtonStyles.chevron}`} />
       Exit
-    </button>
-  )
+    </Link>
+  );
 }
