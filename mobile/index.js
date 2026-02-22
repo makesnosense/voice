6afed9c9e43f5cac525bb0d1a21e9f8833a8802e
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-02-22 03:02:49 +0300

    chore(mobile): use modular api

--- mobile/index.js
+++ mobile/index.js
@@ -1,7 +1,11 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
-import messaging from '@react-native-firebase/messaging';
+import {
+  getMessaging,
+  setBackgroundMessageHandler,
+} from '@react-native-firebase/messaging';
 
-messaging().setBackgroundMessageHandler(async () => {});
+setBackgroundMessageHandler(getMessaging(), async () => {});
+
 AppRegistry.registerComponent(appName, () => App);
