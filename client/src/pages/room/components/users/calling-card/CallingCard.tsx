e63bd7373ebf3cc02e168d0cc06009b95c22a657
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-04-06 04:10:17 +0300

    improvement(client/CallingCard): redesign CallingCard
    - transparent background and red border in declined state
    - smaller display name in CallingCard
    - declined label at the center of remaining space below display name

--- client/src/pages/room/components/users/calling-card/CallingCard.tsx
+++ client/src/pages/room/components/users/calling-card/CallingCard.tsx
@@ -14,29 +14,37 @@ export default function CallingCard({ email, onCancel, declined = false }: Calli
   const displayName = email.split('@')[0];
 
   return (
-    <div className={`${baseStyles.card} ${userCardStyles.userCard}`}>
+    <div
+      className={`${baseStyles.card} ${userCardStyles.userCard} ${callingCardStyles.callingCard} ${declined ? callingCardStyles.declinedCard : ''}`}
+    >
       <div className={userCardStyles.userCardContent}>
-        <span className={`${baseStyles.title} ${userCardStyles.displayName}`}>{displayName}</span>
+        <span className={`${baseStyles.title} ${callingCardStyles.displayName}`}>
+          {displayName}
+        </span>
 
-        <div className={userCardStyles.audioWavesContainer}>
-          {declined ? (
+        {declined ? (
+          <div className={callingCardStyles.declinedContent}>
             <span className={callingCardStyles.declinedLabel}>declined</span>
-          ) : (
-            <div className={callingCardStyles.dots}>
-              <span />
-              <span />
-              <span />
+          </div>
+        ) : (
+          <>
+            <div className={userCardStyles.audioWavesContainer}>
+              <div className={callingCardStyles.dots}>
+                <span />
+                <span />
+                <span />
+              </div>
             </div>
-          )}
-        </div>
 
-        <button
-          className={`${buttonStyles.iconButton} ${buttonStyles.button} ${buttonStyles.lightRed} ${declined ? callingCardStyles.hidden : ''}`}
-          onClick={onCancel}
-          title="dismiss"
-        >
-          <PhoneOff size={16} />
-        </button>
+            <button
+              className={`${buttonStyles.iconButton} ${buttonStyles.button} ${buttonStyles.lightRed}`}
+              onClick={onCancel}
+              title="dismiss"
+            >
+              <PhoneOff size={16} />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
