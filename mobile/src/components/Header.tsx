b96c98b862f91db09f633e3a950acf611191a772
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-04-09 18:52:32 +0300

    improvement(mobile/Header): increase Header vertical padding

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     paddingHorizontal: 20,
-    paddingVertical: 14,
+    paddingVertical: 16,
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderBottomColor: '#e2e8f0',
   },
