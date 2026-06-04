a0672ff36f963cb68f94ae4c23119bfc9acea891
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-06-04 07:18:12 +0300

    chore(typecheck): make new messages not clear previous output but appens instead

--- typecheck.sh
+++ typecheck.sh
@@ -2,6 +2,6 @@
 npx concurrently \
   --names "client,mobile,server" \
   -c "blue,green,yellow" \
-  "cd client && tsc -b --watch" \
-  "cd mobile && tsc -b --watch" \
-  "cd server && tsc -b --watch"
+  "cd client && tsc -b --watch --preserveWatchOutput" \
+  "cd mobile && tsc -b --watch --preserveWatchOutput" \
+  "cd server && tsc --noEmit --watch --preserveWatchOutput"
